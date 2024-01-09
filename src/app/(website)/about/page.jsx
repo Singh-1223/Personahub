818ca9e125b7dc9b3f1644b27bfe0beb4e50db55
Lovepreet singh <lovepreetsingh1223@gmail.com>
2024-01-09 21:48:02 +0530

    bug fix

--- src/app/(website)/about/page.jsx
+++ src/app/(website)/about/page.jsx
@@ -13,9 +13,10 @@ const page = () => {
     <section className="bg-gray-100 px-4 py-8 md:p-10 flex flex-col justify-center items-center">
     <h2 className="text-3xl font-bold text-gray-800 mb-4">Unleash Your Digital Footprint</h2>
     <p className="text-gray-600 text-center md:max-w-2xl">
-      Tired of juggling a dozen social media and professional links? We get it. That's why
-      we built LinkFree, your one-stop shop for showcasing your entire digital presence.
-    </p>
+      Tired of juggling a dozen social media and professional links.  
+       We get it. That is why we built LinkFree, your one-stop shop for showcasing your entire digital presence.
+      
+    </p>  // eslint-disable-line react/no-unescaped-entities
     <div className="mt-8 flex flex-wrap justify-around md:justify-center gap-4">
       <Card
         icon="fa-user"
